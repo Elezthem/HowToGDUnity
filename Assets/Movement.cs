ab3a302799096dbe30c1b0cb7261f41f327349b2
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2024-01-11 15:56:32 +0300

    Add files via upload

---
+++ Assets/Movement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Speeds { Slow = 0, Normal = 1, Fast = 2, Faster = 3, Fastest = 4 };
+public enum Gamemodes { Cube = 0, Ship = 1 };
+
+public class Movement : MonoBehaviour
+{
+    public Speeds CurrentSpeed;
+    public Gamemodes CurrentGamemode;
+    //                       0      1      2       3      4
+    float[] SpeedValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
+
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    public LayerMask GroundMask;
+    public Transform Sprite;
+
+    Rigidbody2D rb;
+
+    int Gravity = 1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.right * SpeedValues[(int)CurrentSpeed] * Time.deltaTime;
+
+        if (rb.velocity.y < -24.2f)
+            rb.velocity = new Vector2(rb.velocity.x, -24.2f);
+
+        Invoke(CurrentGamemode.ToString(), 0);
+    }
+
+    bool OnGround()
+    {
+        return Physics2D.OverlapBox(GroundCheckTransform.position + Vector3.up - Vector3.up * (Gravity - 1 / -2), Vector2.right * 1.1f + Vector2.up * GroundCheckRadius, 0, GroundMask);
+    }
+
+    bool TouchingWall()
+    {
+        return Physics2D.OverlapBox((Vector2)transform.position + (Vector2.right * 0.55f), Vector2.up * 0.8f + (Vector2.right * GroundCheckRadius), 0, GroundMask);
+    }
+
+    void Cube()
+    {
+        print(OnGround());
+
+        if (OnGround())
+        {
+            Vector3 Rotation = Sprite.rotation.eulerAngles;
+            Rotation.z = Mathf.Round(Rotation.z / 90) * 90;
+            Sprite.rotation = Quaternion.Euler(Rotation);
+
+            if (Input.GetMouseButton(0))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * 7.6581f * Gravity, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            Sprite.Rotate(Vector3.back * 2);
+        }
+    }
+
+    void Ship()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * 2);
+
+        if (Input.GetMouseButton(0))
+            rb.gravityScale = -4.314969f;
+        else
+            rb.gravityScale = 4.314969f;
+
+        rb.gravityScale = rb.gravityScale * Gravity;
+    }
+
+    public void ChangeThroughPortal(Gamemodes Gamemode, Speeds Speed, int gravity, int State)
+    {
+        switch (State)
+        {
+            case 0:
+                CurrentSpeed = Speed;
+                break;
+            case 1:
+                CurrentGamemode = Gamemode;
+                break;
+            case 2:
+                Gravity = gravity;
+                rb.gravityScale = Mathf.Abs(rb.gravityScale) * gravity;
+                break;
+        }
+    }
+}
---
+++ Assets/PortalScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    public Gamemodes Gamemode;
+    public Speeds Speed;
+    public bool gravity;
+    public int State;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        try
+        {
+            Movement movement = collision.gameObject.GetComponent<Movement>();
+
+            movement.ChangeThroughPortal(Gamemode, Speed, gravity ? 1 : -1, State);
+        }
+        catch { }
+    }
+}
